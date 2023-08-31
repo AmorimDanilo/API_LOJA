8ad183575a76457cac016940c8c554bec9de7c39
amorimdanilo <danilosousa@aluno.senai.br>
2023-08-31 15:57:39 -0300

    Criando classes para produtos e tabelaProdutos :)

---
+++ loja/src/main/java/com/loja/TabelaProdutos.java
@@ -0,0 +1,26 @@
+package com.loja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabelaProdutos {
+    List<Produto> produtos = new ArrayList<>();
+
+    /**
+     * No construtor a lista de produtos será preenchida
+     */
+
+    public TabelaProdutos() {
+        Produto caneta = new Produto(1, "Caneta BIC", "Caneta BIC na cor azul", 1.99);
+        Produto borracha = new Produto(2, "Borracha Branca", "Borracha branca comum", 3.99);
+        Produto lapis = new Produto(3, "Lápis Faber Castell", "Lapis Faber Castell Comum", 2.99);
+        Produto apontador = new Produto(4, "Apontador", "Apontador comum com reservatório", 7.99);
+        Produto caderno = new Produto(5, "Caderno espiral", "Caderno espiral de 96 folhas", 15.99);
+
+        this.produtos.add(caneta);
+        this.produtos.add(borracha);
+        this.produtos.add(lapis);
+        this.produtos.add(apontador);
+        this.produtos.add(caderno);
+    }
+}
